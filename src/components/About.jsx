222f22ad3bb7d4c208e0dee5796804a79d7e0e82
Pulkit Tyagi <83001524+Pulkit0111@users.noreply.github.com>
2021-09-25 01:10:00 +0530

    updated about info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export function About() {
       <p>
         Have you guys ever been succumbed by the thought of what to watch over
         the weekend,I know i have, to help you getting rid of that confusion I
-        present to you a small project of mine MOVPEDIA, be it a movie or a tv
+        present to you a small project of mine MOVIE_WORM, be it a movie or a tv
         series that you want to binge, just visit here and look for the desired
-        content and you get all the detail relted to the flick you want to
-        enjoy, that will help you decide based on the IMDB amd ROTTEN TOMATOES
+        content and you get all the details related to the flick you want to
+        enjoy, that will help you decide based on the IMDB and ROTTEN TOMATOES
         ratings.
       </p>
       <p>HOPE YOU WILL LIKE MY EFFORT</p>
